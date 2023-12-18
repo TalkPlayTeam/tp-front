e9045d526c7aa8844783daee52902117df4fef7b
Clashix <84671657+Clashix@users.noreply.github.com>
2023-12-18 12:23:31 +0100

    fix: Essaie de corriger bug css

---
+++ src/views/inscription_1.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './inscription.css';
+
+function App() {
+  return (
+    <div className="first_part_inscription">
+      <title>Inscription - Talkplay</title>
+      <header className="title_inscription">
+        <h1>Inscription</h1>
+        <h3>Créez un compte TalkPlay</h3>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
