5401cf2bda6d1d19b92ba96757d77750fdab81e3
Clashix <84671657+Clashix@users.noreply.github.com>
2024-01-02 17:17:39 +0100

    feat : mise en place de la page de connexion

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import InscriptionForm from './views/public/inscription_1';
-import GamesPage from './views/public/inscription_2';
-import Inscription3 from './views/public/inscription_3';
+import InscriptionForm from './views/public/inscription/inscription_1';
+import GamesPage from './views/public/inscription/inscription_2';
+import Inscription3 from './views/public/inscription/inscription_3';
 import './i18n';
+import LoginPage from './views/public/connexion/loginPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,7 +16,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Inscription3 />} />
+        <Route path="/" element={<LoginPage />} />
         {/* <Route path="/" element={<InscriptionForm preloadedEmail={null} />} /> */}
         <Route path="/inscription-2" element={<GamesPage />} />
       </Routes>
---
+++ src/views/public/connexion/loginPage.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import '../styles/loginPage.css'; 
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Gérer la soumission du formulaire
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Traitement de la connexion ici
+    console.log(email, password);
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Connexion</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Mot de passe</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <a href="#" className="forgot-password">Mot de passe oublié?</a>
+        </div>
+        <button type="submit">Connexion</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
